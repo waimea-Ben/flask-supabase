eee89bfab8e515c0cf2cf1e8563f92e47c259df5
Master Benjamin <168691813+waimea-Ben@users.noreply.github.com>
2025-05-21 12:23:15 +1200

    details page

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,27 @@ def home():
     return render_template("pages/home.jinja", things=records)
 
 
+
+@app.get("/thing/<int:id>")
+def showThing(id):
+    response = supabase.table("things").select().eq("id",id).single().execute()
+    record = response.data
+
+    return render_template("pages/thing.jinja", thing=record)
+
+@app.get("/thing/")
+def showThingHome():
+    response = supabase.table("things").select().order("id").execute()
+    records = response.data
+
+    return render_template("pages/thingHome.jinja", things=records)
+
+
+@app.get("/delete/<int:id>")
+def deleteThing(id):
+
+    return
+
 @app.errorhandler(404)
 def notfound(errorhandler):
     return render_template("pages/404.jinja")
